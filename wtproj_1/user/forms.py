824b0e83be399d38c64e09ffe5b52f2f1631d4ad
anandhakris <creattech2000@gmail.com>
2021-07-14 09:06:01 +0530

    login finished

--- wtproj_1/user/forms.py
+++ wtproj_1/user/forms.py
@@ -9,3 +9,12 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name','last_name','username', 'email', 'password1', 'password2']
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'lname'}),
+            'username': forms.TextInput(attrs={'class': 'username'}),
+            'email':forms.TextInput(attrs={'class': 'email'}),
+            'password1':forms.TextInput(attrs={'class': 'password1'}),
+            'password2':forms.TextInput(attrs={'class': 'password2'}),
+        }
